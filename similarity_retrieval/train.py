d49170a3f213ce678dc8ae5a47574d49fc8eb70c
Prakash Sellathurai <prakashsellathurai@gmail.com>
2020-05-25 22:14:03 +0530

    minimal setup

--- similarity_retrieval/train.py
+++
@@ -1,9 +0,0 @@
-from model import LatentModel, get_pretrained_model
-from dataset import download_fashion_mnist
-
-if __name__ == "__main__":
-    training_files = download_fashion_mnist()
-    embedding_model = get_pretrained_model()
-
-    latent_model = LatentModel(embedding_model)
-    latent_model.train(training_files)
---
+++ train.py
@@ -0,0 +1,12 @@
+from similarity_retrieval.model import LatentModel, get_pretrained_model
+from similarity_retrieval.database import download_fashion_mnist
+
+if __name__ == "__main__":
+    (x_train, y_train), (x_test, y_test) = download_fashion_mnist(samples=100)
+    training_files = zip(x_train, y_train)
+    embedding_model = get_pretrained_model()
+
+    latent_model = LatentModel(embedding_model,  force_retrain=True)
+    print("\033[92m training started  \033[0m")
+    latent_model.train(training_files)
+    print("\033[92m training ended  \033[0m")
